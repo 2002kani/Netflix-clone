55cbf28e5ae55ed62716d1ab50c17e8dbdc78935
02Kani <kanikaradag02@gmail.com>
2024-12-24 18:12:41 +0100

    #

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,33 @@
 import "./Login.css"
+import Logo from "../../Assets/logo.png"
 
 const Login = () => {
     return(
         <div className="login">
+            <img src={Logo} className="login-logo"></img>
+            <div className="login-form">
+                <h1> Registrieren </h1>
+                <form>
+                    <input type="text" placeholder="Dein Name"/>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Passwort"/>
+                    <button> Registrieren </button>
+    
+                    <div className="form-hilfe">
+                        <div className="erinnern">
+                            <input type="checkbox" />
+                            <label htmlFor="">Erinnere mich</label>
+                        </div>
+                        <p> Brauchst du Hilfe?</p>
+                    </div>
+                </form>
+                <div className="form-switch">
+                    <p> Neu auf Netflix? <span> Jetzt Registrieren </span></p>
+                    <p> Hast schon ein Account? <span> Jetzt Anmelden </span></p>
+                </div>
+            </div>
         </div>
     );
-}
+} 
 
 export default Login
