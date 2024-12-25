4dac3b2c15a640b24dfa64b45a95c1ffd5056837
02Kani <kanikaradag02@gmail.com>
2024-12-25 15:30:15 +0100

    Update Titlecards.jsx

--- src/Components/Titlecards/Titlecards.jsx
+++ src/Components/Titlecards/Titlecards.jsx
@@ -1,10 +1,19 @@
 import "./Titlecards.css"
 import cards_data from "../../Assets/cards/Cards_data"
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Titlecards = ({title, category}) => {
 
     const cardsRef = useRef();
+    const [apiData, SetApiData] = useState([]);
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGY5ZDA5OTlhODkwNDUyNDgxYjg2MjExNmU5MTFjOSIsIm5iZiI6MTczNTA3MTQ4NS40NTcsInN1YiI6IjY3NmIxNmZkZmRhYTdjMjU0OWE5ZjFkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9bWaMzcq8t8FpHU8eot9uqTektxD3UJRtknfePscX0g'
+        }
+      };
 
     const handleWheel = (event) => {
         event.preventDefault();
@@ -12,6 +21,11 @@ const Titlecards = ({title, category}) => {
     }
 
     useEffect(()=> {
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        .then(res => res.json())
+        .then(res => SetApiData(res.results))
+        .catch(err => console.error(err));
+
         cardsRef.current.addEventListener("wheel", handleWheel)
     }, [])
 
@@ -19,10 +33,10 @@ const Titlecards = ({title, category}) => {
         <div className="title-cards">
             <h2>{title ? title : "Beliebt auf Netflix"}</h2>
             <div className="card-liste" ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return <div className="card" key={index}>
-                        <img src={card.image}></img>
-                        <p>{card.name}</p>
+                        <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}></img>
+                        <p>{card.original_title}</p>
                     </div>
                 })}
             </div>
